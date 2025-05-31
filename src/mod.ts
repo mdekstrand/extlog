f0bda07dd7a71ed32c8d32d5284c9b27dd3d2f8d
Michael Ekstrand <md@ekstrandom.net>
2025-05-31 18:30:40 -0400

    improve performance marks

--- src/mod.ts
+++ src/mod.ts
@@ -5,7 +5,6 @@ import { LogWriter } from "./writer.ts";
 import { addGauge, removeGauge, setupConsole } from "./console.ts";
 import { newContext, TracingContext } from "./context.ts";
 import { type Logger, namedLogger, rootLogger } from "./logger.ts";
-import { timingMark } from "./perf.ts";
 import { type ProgressBar, progressBar } from "./progress.ts";
 import { type LogOptions, setupLogging } from "./setup.ts";
 import { type LogEngine, logEngine } from "./engine.ts";
@@ -20,7 +19,6 @@ export {
   rootLogger,
   setupConsole,
   setupLogging,
-  timingMark,
   TracingContext,
 };
 export type { LogEngine, Logger, LogOptions, LogWriter, ProgressBar };
--- src/perf.ts
+++ src/perf.ts
@@ -1,26 +1,59 @@
 import { TracingContext } from "./context.ts";
 
-export class PerfMark {
+/**
+ * A performance marker for managing performance measurement lifecycles.
+ *
+ * This simplifies use of the JavaScript `performance` tools.  When the mark
+ * is created (with {@link timingMark}), a start marker is created.  When
+ * the {@link measure} method is called or the mark is disposed, timing
+ * since the mark's creation is recorded with `performance.measure`.
+ */
+export interface PerfMark extends Disposable {
+  readonly name: string;
+
+  /**
+   * Take the measurement.
+   * @param detail Measurement details (overrides details provided at mark creation).
+   */
+  measure(detail?: Record<string, unknown>): void;
+}
+
+class PerfMarkImpl implements PerfMark {
   name: string;
   detail?: unknown;
+  measured: boolean = false;
 
   constructor(name: string, detail?: unknown) {
     this.name = name;
     this.detail = detail;
   }
 
+  [Symbol.dispose]() {
+    this.measure();
+  }
+
   measure(detail?: unknown) {
+    if (this.measured) return;
+
     performance.measure(this.name, {
       start: `start:${this.name}`,
       detail: detail ?? this.detail,
     });
+    this.measured = true;
   }
 }
 
+/**
+ * Create a mark for a future performance measurement.
+ *
+ * @param name The name of the performance mark.
+ * @param detail Details to attach to the performance measuremnet.
+ * @returns The mark object.
+ */
 export function timingMark(name: string, detail?: Record<string, unknown>): PerfMark {
   performance.mark(`start:${name}`);
   if (detail?.context && detail.context instanceof TracingContext) {
     detail.context = detail.context.tag;
   }
-  return new PerfMark(name, detail);
+  return new PerfMarkImpl(name, detail);
 }
