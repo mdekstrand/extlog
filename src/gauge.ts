a77272bae0dd74dd2a1fd12dcac564484f43f0a5
Michael Ekstrand <md@ekstrandom.net>
2025-05-31 15:51:55 -0400

    implement gauges

--- src/gauge.ts
+++ src/gauge.ts
@@ -1,10 +1,26 @@
+/**
+ * Interface for “gauges”, display lines at the bottom of the console display.
+ */
 import { EventEmitter } from "@mary/events";
 
+/**
+ * Events emitted by a gauge.
+ */
 export type GaugeEvents = {
+  /**
+   * The gauge has been updated and needs to be refreshed.
+   */
   refresh: [];
+  /**
+   * The gauge is finished and can be removed from the display.
+   */
   finish: [];
 };
 
+/**
+ * Base class for gauges.  Gauges emit events to inform the console of their need to
+ * redisplay, and the console renders them with the {@link render} method.
+ */
 export abstract class Gauge extends EventEmitter<GaugeEvents> {
   /**
    * Render a gauge to a string.  The returned string should *not* include a newline.
